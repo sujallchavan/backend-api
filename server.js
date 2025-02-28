8b8a79857bd5b431b85171ce264c90a6328a8b42
sujallchavan <sujallchavan@gmail.com>
2025-03-01 02:14:14 +0530

    hello

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Connect to MongoDB for Each Database
-const customerDB = mongoose.createConnection(process.env.CUSTOMER_DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const
+const authRoutes = require("./routes/authRoutes");
+app.use("/api", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
