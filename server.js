7f8dd25a2feb050dc1aaf6b033fa0250ea3decdd
sujallchavan <sujallchavan@gmail.com>
2025-03-08 01:48:59 +0530

    hello

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
+      "http://127.0.0.1:5512",
       "https://customerergoasia.netlify.app", // ✅ Added Netlify frontend
     ],
     credentials: true, // ✅ Allow credentials (cookies, sessions)
